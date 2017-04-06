9cc91869c77d4dd929a98347574eacb75ea99b2b
Tommaso Amici <tommaso.amici@gmail.com>
2017-04-06 14:29:14 +0200

    js

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-  $grid.masonry({
-    // options...
-  });
+var $grid = $('.grid').imagesLoaded(function () {
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item'
+    });
 });
