8ab6b8adba1eabddf14d76f0d9337b979981b655
Tommaso Amici <tommaso.amici@gmail.com>
2017-04-06 14:41:30 +0200

    js fix

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
+  columnWidth: 40%,
 });
 // layout Isotope after each image loads
 $grid.imagesLoaded().progress( function() {
